0cba8db3b2b6834e21bd164bc176f35768918727
jason <jason@eatekiMac-mini.local>
2015-05-11 17:07:20 +0800

    g

--- JasonBlog/JasonBlog/Class/Controller/Home/HomeViewController.m
+++ JasonBlog/JasonBlog/Class/Controller/Home/HomeViewController.m
@@ -9,6 +9,8 @@
 #import "HomeViewController.h"
 #import "HomeCell.h"
 #import "HomeDetailViewController.h"
+#import "DataRepeater.h"
+
 @interface HomeViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *tab_Main;
 @property(strong, nonatomic) NSMutableArray *marr_Data;
@@ -22,61 +24,64 @@
     self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationController.navigationBar.translucent = YES;
+    
+    
 //    [[RequestManager sharedInstance] sendRequestSuccess:^(id response) {
 //        NSLog(@"%@",response);
 //    } error:^(NSError *responseError) {
 //        NSLog(@"%@",responseError);
 //    }];
-//    
     
     self.marr_Data = [NSMutableArray array];
     
-    //Test GCD
-    NSArray *arr_Image = @[@"http://img0.bdstatic.com/img/image/6446027056db8afa73b23eaf953dadde1410240902.jpg",@"http://e.hiphotos.baidu.com/image/w%3D230/sign=6a4132c09252982205333ec0e7cb7b3b/b17eca8065380cd7bd41aceaa344ad345982815d.jpg",@"http://img0.bdstatic.com/img/image/c6774aeee9cc323de700edcf4f2a40781409804177.jpg",@"http://img0.bdstatic.com/img/image/2043d07892fc42f2350bebb36c4b72ce1409212020.jpg",@"http://d.hiphotos.baidu.com/image/w%3D230/sign=12d2ba1bc45c1038247ec9c18210931c/e61190ef76c6a7ef35991227fffaaf51f3de6670.jpg",@"http://f.hiphotos.baidu.com/image/w%3D230/sign=c43473826e81800a6ee58e0d813433d6/d31b0ef41bd5ad6ec8a8eee783cb39dbb6fd3c6d.jpg",@"http://img0.bdstatic.com/img/image/f46ce32bc44e3f46285ef487689088111408331743.jpg",@"http://c.hiphotos.baidu.com/image/w%3D230/sign=ed49463a37fae6cd0cb4ac623fb20f9e/f9198618367adab4308fbc4e89d4b31c8701e464.jpg",@"http://h.hiphotos.baidu.com/image/w%3D230/sign=a0226c1540a7d933bfa8e3709d4ad194/8ad4b31c8701a18b2847444e9c2f07082838fe07.jpg"];
+    //GCD
+    NSArray *arr_Image = @[@"http://img0.bdstatic.com/img/image/6446027056db8afa73b23eaf953dadde1410240902.jpg",
+                           @"http://e.hiphotos.baidu.com/image/w%3D230/sign=6a4132c09252982205333ec0e7cb7b3b/b17eca8065380cd7bd41aceaa344ad345982815d.jpg",
+                           @"http://img0.bdstatic.com/img/image/c6774aeee9cc323de700edcf4f2a40781409804177.jpg",
+                           @"http://img0.bdstatic.com/img/image/2043d07892fc42f2350bebb36c4b72ce1409212020.jpg",
+                           @"http://d.hiphotos.baidu.com/image/w%3D230/sign=12d2ba1bc45c1038247ec9c18210931c/e61190ef76c6a7ef35991227fffaaf51f3de6670.jpg",
+                           @"http://f.hiphotos.baidu.com/image/w%3D230/sign=c43473826e81800a6ee58e0d813433d6/d31b0ef41bd5ad6ec8a8eee783cb39dbb6fd3c6d.jpg",
+                           @"http://img0.bdstatic.com/img/image/f46ce32bc44e3f46285ef487689088111408331743.jpg",
+                           @"http://c.hiphotos.baidu.com/image/w%3D230/sign=ed49463a37fae6cd0cb4ac623fb20f9e/f9198618367adab4308fbc4e89d4b31c8701e464.jpg",
+                           @"http://h.hiphotos.baidu.com/image/w%3D230/sign=a0226c1540a7d933bfa8e3709d4ad194/8ad4b31c8701a18b2847444e9c2f07082838fe07.jpg",
+                           @"http://a.hiphotos.baidu.com/image/pic/item/77094b36acaf2edd93f23ba08d1001e939019373.jpg"];
     
+    __weak typeof(HomeViewController*) weself = self;
     for(NSString *str_ImageUrl in arr_Image)
     {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), ^{
-            __block typeof(UIImage*) image = nil;
             dispatch_async (dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), ^{
                 NSURL *url = [NSURL URLWithString:str_ImageUrl];
-                NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
-                NSError *downloadError = nil;
-                NSData *imageData = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:nil error:&downloadError];
-
-                if(downloadError == nil && imageData != nil)
+                //加载图片
+                NSData* imageData = [NSData dataWithContentsOfURL: url];
+                if(imageData != nil)
                 {
-                    image = [UIImage imageWithData:imageData];
-                    [self.marr_Data addObject:image];
-                    NSLog(@"Width%f-----Height%f",image.size.width,image.size.height);
-                }
-                else if(downloadError != nil)
-                {
-                    NSLog(@"Error happened = %@", downloadError);
+                    UIImage *image = [UIImage imageWithData:imageData];
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        if (image != nil)
+                        {
+                            //有图片添加到数组并刷新列表
+                            [weself.marr_Data addObject:image];
+                            [weself.tab_Main reloadData];
+                        }
+                        else
+                        {
+                            NSLog(@"没有图片");
+                            [weself.marr_Data addObject:nil];
+                        } 
+                    });
                 }
                 else
                 {
                     NSLog(@"No data could get downloaded the URL.");
                 }
             });
-                dispatch_sync(dispatch_get_main_queue(), ^{
-                    if (image != nil)
-                    {
-                        UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
-                        [imageView setImage:image];
-                        [imageView setContentMode:UIViewContentModeScaleAspectFit];
-                        [self.view addSubview:imageView];
-                    }
-                    else
-                    {
-                        NSLog(@"没有图片");
-                        
-                    } 
-            });
-            
         });
     }
     
+    
+    //NSPredicate NSRegularExpression
+//    NSPredicate *predicate = [NSPredicate predicateWithFormat:<#(NSString *), ...#>]
 
 }
 
@@ -89,12 +94,12 @@
 #pragma mark - UITableviewDelegate && UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return self.marr_Data.count;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -134,7 +139,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    UIImage *image = [self.marr_Data objectAtIndex:indexPath.row];
+    NSLog(@"%lu",(unsigned long)self.marr_Data.count);
     HomeDetailViewController *detailVC = [[HomeDetailViewController alloc] init];
     detailVC.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:detailVC animated:YES];
